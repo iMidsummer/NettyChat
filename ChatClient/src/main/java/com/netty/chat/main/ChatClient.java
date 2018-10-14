81c13d53902a4c27fa530c7ce69b5e02ecc1a9d3
Wang <chwan@amazon.com>
2018-10-13 18:37:47 -0700

    Initial commit

---
+++ ChatClient/src/main/java/com/netty/chat/main/ChatClient.java
@@ -0,0 +1,7 @@
+package com.netty.chat.main;
+
+public class ChatClient {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello client");
+    }
+}
---
+++ ChatServer/src/main/java/com/netty/chat/main/ChatServer.java
@@ -0,0 +1,7 @@
+package com.netty.chat.main;
+
+public class ChatServer {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello server");
+    }
+}
