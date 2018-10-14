8595f00f9b375e52896a5ec8bccd8be15d21d22e
Wang <chwan@amazon.com>
2018-10-13 19:22:52 -0700

    Implement the basic bootstraps of Server and Client

--- ChatClient/src/main/java/com/netty/chat/main/ChatClient.java
+++ ChatClient/src/main/java/com/netty/chat/main/ChatClient.java
@@ -1,7 +1,29 @@
 package com.netty.chat.main;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class ChatClient {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello client");
+        NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                            System.out.println("client connected");
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 3600).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            eventLoopGroup.shutdownGracefully().sync();
+        }
     }
 }
--- ChatServer/src/main/java/com/netty/chat/main/ChatServer.java
+++ ChatServer/src/main/java/com/netty/chat/main/ChatServer.java
@@ -1,7 +1,40 @@
 package com.netty.chat.main;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class ChatServer {
     public static void main(String[] args) throws Exception {
-        System.out.println("hello server");
+        EventLoopGroup bossLoopGroup = new NioEventLoopGroup();
+        EventLoopGroup workerLoopGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossLoopGroup, workerLoopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioServerSocketChannel nioServerSocketChannel) throws Exception {
+                            System.out.println("server starts to listen");
+                        }
+                    })
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                            System.out.println("init child handler");
+                        }
+                    });
+
+            ChannelFuture future = serverBootstrap.bind(3600).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossLoopGroup.shutdownGracefully().sync();
+            workerLoopGroup.shutdownGracefully().sync();
+        }
     }
 }
